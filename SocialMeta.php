ef8130c7f68253fc8bc1445065a9c3c06b53ef67
Rousselle <ylan.rousselle76@gmail.com>
2022-03-14 12:08:26 +0100

    add install function

--- SocialMeta.php
+++ SocialMeta.php
@@ -6,10 +6,7 @@ class SocialMeta {
     \* ------------------------------------------------ */
     // ___ VAR ___
 
-    private $fileParam = [
-            "img_folder_parent" => ".",
-            "img_format_ignore" => false
-    ];
+    private $fileParam;
 
     private $commonParam = [
             "url" => null,
@@ -33,6 +30,17 @@ class SocialMeta {
 
     // ___ FUNCTION ___
 
+    private function remote_file_exists($url) {
+        ini_set('allow_url_fopen', '1');
+        try {
+            @fclose(@fopen($url, 'r'));
+            $exists =  true;
+        } catch (\Throwable $th) {
+            $exists =  false;
+        }
+        return $exists;
+    }
+
     /* ------------------------------------------------ *\
             Public
     \* ------------------------------------------------ */
@@ -44,7 +52,24 @@ class SocialMeta {
 
     // ___ FUNCTION ___
 
+    public static function install(){
+        
+        $fileParam = [
+            "img_folder_parent" => ".",
+            "img_param" => [
+                "img_format_ignore" => false,
+                "default_img" => null
+            ]
+        ];
+
+        if (!file_exists("socialMeta_param.json")) {
+            file_put_contents("socialMeta_param.json", json_encode($fileParam));
+        }
+    }
+
     public function __construct(string $url, string $title, string $description) {
+        $this->fileParam = json_decode(file_get_contents("socialMeta_param.json"), 1);
+
         $ifp = $this->fileParam["img_folder_parent"];
         $ifp_given = ($ifp == false) ? throw new Exception("<strong> You have to set the images folder parent on line 10</strong>", 1) : true;
         
@@ -68,17 +93,61 @@ class SocialMeta {
      * Setter
      */
 
+    public function setGoogleImg (string $url){
+        
+    }
+
+    public function setOgImg (string $url){
+        
+    }
+
+    public function setTwitterImg (string $url){
+        
+    }
     
     /**
      * Getter
      */
 
-    public function checkList(){
+    public function getCheckList(){
 
+    }
+
+    public function print() {
+        $url = $this->commonParam['url'];
+        $title = $this->commonParam['title'];
+        $description = $this->commonParam['description'];
+
+
+        $print = "<!-- Google+ -->";
+	    $print .= "<meta itemprop='name'             content='".$title."'>";
+	    $print .= "<meta itemprop='description'      content='".$description."'>";
+	    
+        $print .= "<meta itemprop='image'            content='"."d"."'>";
+
+	    $print .= "<!-- Facebook -->";
+        $print .= "<meta property='og:title'         content='".$title."' />";
+	    $print .= "<meta property='og:url'           content='".$url."' />";
+        $print .= "<meta property='og:description'   content='".$description."' />";
+
+        $print .= "<meta property='og:image'         content='"."d"."'>";
+
+        $print .= "<meta property='og:type'          content='website' />";
+
+        $print .= "<!-- Twitter -->";
+        $print .= "<meta name='twitter:title'         content='".$title."'>";
+        $print .= "<meta name='twitter:url'           content='".$url."' />";
+        $print .= "<meta name='twitter:description'   content='".$description."'>";
+
+        $print .= "<meta name='twitter:image'         content='"."d"."'>";
+
+        $print .= "<meta name='twitter:card'          content='summary_large_image'>";
+        
+        echo htmlspecialchars($print);
     }
     
 }
 
-$meta = new SocialMeta("http://www.ylanrousselle.fr/", "Portfolio", "Bienvenue sur mon portfolio");
+
 
 ?>
