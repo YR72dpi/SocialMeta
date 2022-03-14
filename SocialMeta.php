cc9d8c0309a96aaa13179242eea8324624f82f50
Rousselle <ylan.rousselle76@gmail.com>
2022-03-14 13:22:35 +0100

    add setter function

--- SocialMeta.php
+++ SocialMeta.php
@@ -41,6 +41,25 @@ class SocialMeta {
         return $exists;
     }
 
+    private function fileFind($url) {
+        if (preg_match("/^https?:\/\/.*(\..*)/i", $url)) {
+            if ($this->remote_file_exists($url)) {
+                $this->SocialImages['google_img'] = $url;
+                return $url;
+            }else {
+                return false;
+            }
+        } else {
+            $path = $this->fileParam["img_folder_parent"].$this->fileParam["img_param"]["default_img"]."/socialMeta_img/".$url;
+            if (file_exists($path)) {
+                $this->SocialImages['google_img'] = $path;
+                return $path;
+            }else {
+                return false;
+            }
+        }
+    }
+
     /* ------------------------------------------------ *\
             Public
     \* ------------------------------------------------ */
@@ -55,10 +74,10 @@ class SocialMeta {
     public static function install(){
         
         $fileParam = [
-            "img_folder_parent" => ".",
+            "img_folder_parent" => "false",
             "img_param" => [
                 "img_format_ignore" => false,
-                "default_img" => null
+                "default_img" => ""
             ]
         ];
 
@@ -69,9 +88,10 @@ class SocialMeta {
 
     public function __construct(string $url, string $title, string $description) {
         $this->fileParam = json_decode(file_get_contents("socialMeta_param.json"), 1);
-
+        
+        // Verify if the images folder parent exists
         $ifp = $this->fileParam["img_folder_parent"];
-        $ifp_given = ($ifp == false) ? throw new Exception("<strong> You have to set the images folder parent on line 10</strong>", 1) : true;
+        $ifp_given = ($ifp == "false") ? throw new Exception("You have to set the images folder parent on line 10", 1) : true;
         
         if ($ifp_given && file_exists($ifp)) {
             if (!file_exists($ifp."/socialMeta_img")) {
@@ -94,23 +114,52 @@ class SocialMeta {
      */
 
     public function setGoogleImg (string $url){
-        
+        $file = $this->fileFind($url);
+        if ($file) {
+            $this->SocialImages["google_img"] = $file;
+            return true;
+        } else {
+            throw new Exception("No file find", 1);
+        }
     }
 
     public function setOgImg (string $url){
-        
+        $file = $this->fileFind($url);
+        if ($file) {
+            $this->SocialImages["og_img"] = $file;
+            return true;
+        } else {
+            throw new Exception("No file find", 1);
+        }
     }
 
     public function setTwitterImg (string $url){
-        
+        $file = $this->fileFind($url);
+        if ($file) {
+            $this->SocialImages["twitter_twitter"] = $file;
+            return true;
+        } else {
+            throw new Exception("No file find", 1);
+        }
     }
     
+    public function setSameImg (string $url) {
+        $file = $this->fileFind($url);
+        if ($file) {
+            $this->SocialImages["google_img"] = $file;
+            $this->SocialImages["og_img"] = $file;
+            $this->SocialImages["twitter_img"] = $file;
+            return true;
+        } else {
+            throw new Exception("No file find", 1);
+        }
+    }
+
     /**
      * Getter
      */
 
     public function getCheckList(){
-
     }
 
     public function print() {
@@ -123,25 +172,25 @@ class SocialMeta {
 	    $print .= "<meta itemprop='name'             content='".$title."'>";
 	    $print .= "<meta itemprop='description'      content='".$description."'>";
 	    
-        $print .= "<meta itemprop='image'            content='"."d"."'>";
+        $print .= "<meta itemprop='image'            content='".$this->SocialImages["google_img"]."'>";
 
 	    $print .= "<!-- Facebook -->";
         $print .= "<meta property='og:title'         content='".$title."' />";
 	    $print .= "<meta property='og:url'           content='".$url."' />";
         $print .= "<meta property='og:description'   content='".$description."' />";
 
-        $print .= "<meta property='og:image'         content='"."d"."'>";
+        $print .= "<meta property='og:image'         content='".$this->SocialImages["og_img"]."'>";
 
         $print .= "<meta property='og:type'          content='website' />";
 
         $print .= "<!-- Twitter -->";
-        $print .= "<meta name='twitter:title'         content='".$title."'>";
-        $print .= "<meta name='twitter:url'           content='".$url."' />";
-        $print .= "<meta name='twitter:description'   content='".$description."'>";
+        $print .= "<meta name='twitter:title'        content='".$title."'>";
+        $print .= "<meta name='twitter:url'          content='".$url."' />";
+        $print .= "<meta name='twitter:description'  content='".$description."'>";
 
-        $print .= "<meta name='twitter:image'         content='"."d"."'>";
+        $print .= "<meta name='twitter:image'        content='".$this->SocialImages["twitter_img"]."'>";
 
-        $print .= "<meta name='twitter:card'          content='summary_large_image'>";
+        $print .= "<meta name='twitter:card'         content='summary_large_image'>";
         
         echo htmlspecialchars($print);
     }
